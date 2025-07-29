d6d599cf999d501729eacf96fdbd90d908b277a3
Kiolope <knespero@gmail.com>
2025-07-29 20:00:06 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  target: 'serverless',
   reactStrictMode: true,
-  env: {
-    NODE_OPTIONS: '--openssl-legacy-provider',
-  },
 };
 
 module.exports = nextConfig;
